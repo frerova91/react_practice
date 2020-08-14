4975f012a277d39b4021ecb6873cfabc39b016a7
unrealgraw <frerova91@gmail.com>
2020-08-13 23:01:59 -0400

    linter config & jsconfig for relative path in the proyect

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
 export const App = () => {
-    return (
-        <div>
-            <h1>HOLA MUNDO</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1>HOLA MUNDO</h1>
+    </div>
+  )
 }
